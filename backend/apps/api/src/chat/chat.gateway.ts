226ba8e2a7072c7350802a50e6122930442097f8
henyumino <henyumino37@gmail.com>
2024-04-05 15:30:37 +0800

    implement private notif

--- backend/apps/api/src/chat/chat.gateway.ts
+++ backend/apps/api/src/chat/chat.gateway.ts
@@ -1,6 +1,14 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer, WsResponse } from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  WsResponse,
+} from '@nestjs/websockets';
 import { Observable } from 'rxjs';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({
   cors: {
@@ -11,10 +19,34 @@ export class ChatGateway {
   @WebSocketServer()
   server: Server;
 
+  private logger = new Logger('chat gateway');
+
+  private onlineUsers = [];
+
   @SubscribeMessage('send_chat')
   handleMessage(@MessageBody() data: any): Observable<WsResponse<string>> {
-    console.log(data)
-    this.server.emit('get_chat', data)
+    console.log(data);
+    this.server.emit('get_chat', data);
     return data;
+  }
+
+  @SubscribeMessage('newOnlineUser')
+  addOnlineUser(@MessageBody() data: any, @ConnectedSocket() client: Socket): any {
+    // NOTE add user ketika on masuk ke app
+    !this.onlineUsers.some((user) => user.username === data.username) &&
+      this.onlineUsers.push({ username: data, socket_id: client.id });
+    // this.logger.debug(data, client);
+  }
+
+  @SubscribeMessage('getOnlineUser')
+  getOnlineUser(){
+    this.logger.debug("online user:",this.onlineUsers)
+  }
+
+  @SubscribeMessage('sendNotif')
+  sendNotif(@MessageBody() data: any){
+    const user = this.onlineUsers.find((user) => data === user.username);
+    this.server.to(user.socket_id).emit('getNotif', `hey ${data}`)
+    // TODO implementasi ke frontend untuk lonceng notif
   }
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,56 +1,114 @@
-import { useEffect, useState } from 'react'
-// import './App.css'
-import { io } from 'socket.io-client';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { io } from "socket.io-client";
+import { Input } from "@material-tailwind/react";
+import NavbarDark from "./components/Navbar";
 
-const socket = io('http://localhost:5000');
+const socket = io("http://localhost:5000");
 
-function App() {
-  const [msg, setMsg] = useState<any>([])
-  const [data, setData] = useState<any>('')
-  const [isConnected, setIsConnected] = useState(socket.connected);
+export default function App() {
+	const [msg, setMsg] = useState<any>([]);
+	const [data, setData] = useState<any>("");
+	const [isConnected, setIsConnected] = useState(socket.connected);
+	const [user, setUser] = useState<any>("");
+	const [username, setUsername] = useState("");
+	const [receiver, setReceiver] = useState("");
 
-  function sendMsg(){
-    socket.emit('send_chat', {id: `${Math.random()}`, msg: data})
-    setData('')
+	// function sendMsg() {
+	// 	socket.emit("send_chat", { id: `${Math.random()}`, msg: data });
+	// 	setData("");
+	// }
+
+	function getOnlineUser() {
+		socket.emit("getOnlineUser", (data) => console.log(data));
+	}
+
+  function sendNotif(){
+    // send notif
+    socket.emit('sendNotif', receiver)
   }
 
+	useEffect(() => {
+		if (user !== "") {
+			console.log("triger");
+			socket.emit("newOnlineUser", user);
+		}
+		return () => {
+			// socket.off("newUserOnline");
+		};
+	}, [user]);
 
-  useEffect(() => {
-    function onConnect() {
-      setIsConnected(true);
-    }
+	useEffect(() => {
+		function onConnect() {
+			setIsConnected(true);
+		}
 
-    function onDisconnect() {
-      setIsConnected(false);
-    }
+		function onDisconnect() {
+			setIsConnected(false);
+		}
 
-    socket.on('connect', onConnect);
-    socket.on('disconnect', onDisconnect);
-    socket.on('get_chat', (data) => {
-      setMsg((prev: any) => [...prev, data])
-    });
+		socket.on("connect", onConnect);
+		socket.on("disconnect", onDisconnect);
+		socket.on("getNotif", (data: any) => {
+			console.log(data)
+		});
 
-    return () => {
-      socket.off('connect', onConnect);
-      socket.off('disconnect', onDisconnect);
-      socket.off('get_chat');
-    };
-  }, [])
-  
-  // console.log(msg)
+		return () => {
+			socket.off("connect", onConnect);
+			socket.off("disconnect", onDisconnect);
+			socket.off("getNotif"); 
+		};
+	}, []);
 
-  return (
-    <>
-      {isConnected ? 'connected': 'not connect'}
-      <div>
-        {msg.map((el: any,i: number) => <div key={i}>{el.msg}</div>)}
-      </div>
-      <div>
-        <input type="text" value={data} onChange={({target}) => setData(target.value)} />
-        <button onClick={sendMsg}>send</button>
-      </div>
-    </>
-  )
+	// console.log(user);
+	// NOTE : abaikan type component yang tidak satifies
+
+	return (
+		<>
+			<NavbarDark />
+			<p className="text-2xl text-red-500">asd</p>
+			{isConnected ? "connected" : "not connect"}
+			<div>
+				{msg.map((el: any, i: number) => (
+					<div key={i}>{el.msg}</div>
+				))}
+			</div>
+			<div>
+				<div className="w-1/4 m-auto">
+					<Input
+						variant="static"
+						label="username"
+						value={username}
+						onChange={({ target }) => setUsername(target.value)}
+					/>
+					<button
+						onClick={() => {
+							if (username !== "") {
+								setUser(username);
+							}
+						}}
+						className="border border-gray-200 p-2"
+					>
+						add user
+					</button>
+				</div>
+				<button onClick={getOnlineUser}>get online user</button>
+			</div>
+			<div className="m-auto w-1/4">
+				<Input
+          label="receiver id"
+					variant="static"
+					type="text"		
+					value={receiver}
+					onChange={({ target }) => setReceiver(target.value)}
+				/>
+        <button onClick={sendNotif}>send notif</button>
+			</div>
+		</>
+	);
 }
 
-export default App
+// function getDate() {
+// 	// const time = new Date();
+// 	return Date.now();
+// }
